a118d0d79af65160a356728e0450f32edbeeb97f
matthiasmeissen <meissenmatthias@gmail.com>
2025-11-23 18:50:27 +0100

    Disect code into functions and make main clean

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,25 @@ use std::{fs, path::Path};
 use pulldown_cmark;
 use tera::{Context, Tera};
 
+
+const CONTENT_DIR: &str = "src/content";
+const STYLE_DIR: &str = "src/styles";
+const OUTPUT_DIR: &str = "dist";
+const TEMPLATE_DIR: &str = "src/templates/**/*.html";
+
+
 fn main() {
-    let content_path = "src/content/test.md";
-    let style_path = "src/styles/global.css";
-    let output_dir = "dist";
+    let tera = Tera::new(TEMPLATE_DIR).unwrap();
 
-    // Clear and create output dir
-    if Path::new(output_dir).exists() {
-        fs::remove_dir_all(output_dir).expect("Failed to clear output directory.");
-    }
-    fs::create_dir_all(output_dir).expect("Failed to create dist directory.");
+    prepare_output_dir();
 
-    // Read md file, convert to html and insert into template
-    let md_file = read_md_file(content_path);
-    let raw_html = md_to_html(&md_file);
-    let final_html = html_to_template(&raw_html, "index.html");
+    process_md_to_html_file("test.md", "index.html", "index.html", &tera);
 
-    // Create new html file in output dir
-    fs::write(format!("{output_dir}/index.html"), final_html).expect("Could not write html file.");
-
-    // Copy styles to output dir
-    fs::copy(style_path, format!("{output_dir}/global.css")).expect("Could not copy css file.");
+    copy_css_files("global.css");
 }
+
+
+// Markdown to html
 
 fn read_md_file(input_path: &str) -> String {
     fs::read_to_string(input_path).expect("Could not read md file")
@@ -37,11 +34,33 @@ fn md_to_html(md: &str) -> String {
     html_output
 }
 
-fn html_to_template(input_html: &str, html_template: &str) -> String {
-    let tera = Tera::new("src/templates/**/*.html").unwrap();
-
+fn html_to_template(input_html: &str, html_template: &str, tera: &Tera) -> String {
     let mut context = Context::new();
     context.insert("content", &input_html);
 
     tera.render(html_template, &context).unwrap()
+}
+
+fn process_md_to_html_file(file_name: &str, template_name: &str, target_name: &str, tera: &Tera) {
+    let md_file = read_md_file(format!("{CONTENT_DIR}/{file_name}").as_str());
+    let raw_html = md_to_html(&md_file);
+    let final_html = html_to_template(&raw_html, template_name, tera);
+
+    fs::write(format!("{OUTPUT_DIR}/{target_name}"), final_html).expect("Could not write html file.");
+}
+
+
+// Utilities
+
+fn prepare_output_dir() {
+    if Path::new(OUTPUT_DIR).exists() {
+        fs::remove_dir_all(OUTPUT_DIR).expect("Failed to clear output directory.");
+    }
+    fs::create_dir_all(OUTPUT_DIR).expect("Failed to create dist directory.");
+}
+
+fn copy_css_files(file_name: &str) {
+    let source = format!("{STYLE_DIR}/{file_name}");
+    let dest = format!("{OUTPUT_DIR}/{file_name}");
+    fs::copy(source, dest).expect("Could not copy css file.");
 }
