d3af88cc5f6023ca4ac4c73de54d80088fd36054
matthiasmeissen <meissenmatthias@gmail.com>
2025-11-23 18:21:51 +0100

    Add basic md file -> raw html -> templated html pipeline and output dir with styles

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,33 @@
 
-use std::fs;
+use std::{fs, path::Path};
 use pulldown_cmark;
 use tera::{Context, Tera};
 
 fn main() {
+    let content_path = "src/content/test.md";
+    let style_path = "src/styles/global.css";
+    let output_dir = "dist";
 
-    let mut tera = Tera::new("src/templates/**/*.html").unwrap();
-    tera.add_raw_template("test_template", "Hello, {{ content }}!").unwrap();
+    // Clear and create output dir
+    if Path::new(output_dir).exists() {
+        fs::remove_dir_all(output_dir).expect("Failed to clear output directory.");
+    }
+    fs::create_dir_all(output_dir).expect("Failed to create dist directory.");
 
-    let html_output = file_to_html("src/content/test.md");
+    // Read md file, convert to html and insert into template
+    let md_file = read_md_file(content_path);
+    let raw_html = md_to_html(&md_file);
+    let final_html = html_to_template(&raw_html, "index.html");
 
-    let mut context = Context::new();
-    context.insert("content", &html_output);
+    // Create new html file in output dir
+    fs::write(format!("{output_dir}/index.html"), final_html).expect("Could not write html file.");
 
-    let page = tera.render("test_template", &context).unwrap();
-    let page1 = tera.render("test.html", &context).unwrap();
+    // Copy styles to output dir
+    fs::copy(style_path, format!("{output_dir}/global.css")).expect("Could not copy css file.");
+}
 
-    println!("
-        Raw HTML: {}
-        \nInline Template: {} 
-        \nExternal Template: {}", 
-        html_output, page, page1
-    );
+fn read_md_file(input_path: &str) -> String {
+    fs::read_to_string(input_path).expect("Could not read md file")
 }
 
 fn md_to_html(md: &str) -> String {
@@ -31,7 +37,11 @@ fn md_to_html(md: &str) -> String {
     html_output
 }
 
-fn file_to_html(input_path: &str) -> String {
-    let md_file = fs::read_to_string(input_path).expect("Could not read file");
-    md_to_html(&md_file)
+fn html_to_template(input_html: &str, html_template: &str) -> String {
+    let tera = Tera::new("src/templates/**/*.html").unwrap();
+
+    let mut context = Context::new();
+    context.insert("content", &input_html);
+
+    tera.render(html_template, &context).unwrap()
 }
